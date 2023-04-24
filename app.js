84e5d1eabee8a50c50f6f5fe92bccc9311c184f7
seiyu-nico <yuseiishii141@gmail.com>
2023-04-24 19:46:37 +0900

    a

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const createPlaces = ({ latitude, longitude }) =>
   [
-    ["p1", -3, -3],
-    ["p2", -3, 3],
-    ["p3", 3, -3],
-    ["p4", 3, 3],
+    ["p1", -2, -2],
+    ["p2", -2, 2],
+    ["p3", 2, -2],
+    ["p4", 2, 2],
   ].map(([label, cy, cx]) => ({
     label,
     location: {
